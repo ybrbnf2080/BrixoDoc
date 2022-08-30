bd2c0836c26981014c68743ddea2ea31b0d4fa1f
SiriusMklain <sergeimakarenko5991@gmail.com>
2022-08-31 01:18:56 +0300

    edit static

--- BrixoDoc/settings.py
+++ BrixoDoc/settings.py
@@ -136,19 +136,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_URL = '/media/'
 #
 #
-if DEBUG == True:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
 
 
 # Default primary key field type

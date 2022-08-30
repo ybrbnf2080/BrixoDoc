0d2f6ea72e56d0ff51152933a80a96b5c0356db8
SiriusMklain <sergeimakarenko5991@gmail.com>
2022-08-30 18:40:05 +0300

    add setting and env and docker

--- BrixoDoc/settings.py
+++ BrixoDoc/settings.py
@@ -86,23 +86,23 @@ WSGI_APPLICATION = 'BrixoDoc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-#         'NAME': os.environ.get("SQL_DATABASE", "sqlite3"),
-#         'USER': os.environ.get("SQL_USER", "debt_user"),
-#         'PASSWORD': os.environ.get("SQL_PASSWORD", "123456"),
-#         'HOST': os.environ.get("SQL_HOST", "localhost"),
-#         'PORT': os.environ.get("SQL_PORT", "5432"),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        'NAME': os.environ.get("SQL_DATABASE", "db.sqlite3"),
+        'USER': os.environ.get("SQL_USER"),
+        'PASSWORD': os.environ.get("SQL_PASSWORD"),
+        'HOST': os.environ.get("SQL_HOST"),
+        'PORT': os.environ.get("SQL_PORT"),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
